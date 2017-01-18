54377627a28e4d1ac3c83077698dd173df9fbcd9
Tomas Sivicki <tomas.sivicki@gmail.com>
2017-01-18 13:33:00 +0100

    increased timeout for electron tests (win_x64 build sometimes fails tests)

--- test/e2e/specs/Electron.spec.js
+++ test/e2e/specs/Electron.spec.js
@@ -6,10 +6,13 @@ describe('Electron UI', function () {
   beforeEach(utils.beforeEach)
   afterEach(utils.afterEach)
 
-  it('shows the proper application title', function () {
+  it('shows the proper application title', function (done) {
+    this.timeout(10000);
+
     return this.app.client.getTitle()
       .then(title => {
         expect(title).to.equal(name)
+	done()
       })
   })
 })
